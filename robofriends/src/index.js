901aa9e0cfa9ea64cf67d647b23a710acac909e0
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-07 22:36:58 -0600

    Begin react app.

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={`Hello, Crazy Dude!`}/>, document.getElementById('root'));
 registerServiceWorker();

99b2376bf24acecddb44ab15b19d856e49a09def
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-08 08:34:57 -0600

    Finish creating react app video 1.

---
+++ robofriends/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jeff Fredericks',
+    userName: 'Jeff',
+    email: 'jeff@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Michelle Fredericks',
+    userName: 'Michelle',
+    email: 'michelle@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Alec Fredericks',
+    userName: 'Alec',
+    email: 'alec@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Chelise Fredericks',
+    userName: 'Chelise',
+    email: 'chelise@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Dallin Fredericks',
+    userName: 'Dallin',
+    email: 'dallin@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Preston Fredericks',
+    userName: 'Preston',
+    email: 'preston@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Jet Fredericks',
+    userName: 'Jet',
+    email: 'jet@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Richard Fredericks',
+    userName: 'Rich',
+    email: 'rich@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Darlene Fredericks',
+    userName: 'Darlene',
+    email: 'darlene@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Tami Spraycar',
+    userName: 'Tami',
+    email: 'tami@gmail.com'
+  }
+]

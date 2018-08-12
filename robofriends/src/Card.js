99a2f9cbcff9f13259916993afb73a9ee7931de3
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-11 19:23:06 -0600

    Finish building a react app video 4

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({name, email, id}) => {
+const Card = ({name, house, birthday}) => {
   return(
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+      <img src={`https://robohash.org/${name}?200x200`} alt='robots'/>
       <div>
         <h2>{name}</h2>
-        <p>{email}</p>
+        <p>Birthday: {birthday}</p>
+        <p>House: {house}</p>
       </div>
     </div>
   );
--- robofriends/src/SearchBox.js
+++ robofriends/src/SearchBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange}) => {
   return(
     <div className='pa2'>
-      <input type='search' placeholder='Search Robots'/>  
+      <input 
+        className='pa3 ba b--grean bg-lightest-blue'
+        type='search' 
+        placeholder='Search Robots'
+        onChange={searchChange}
+      />  
     </div>
     
   );

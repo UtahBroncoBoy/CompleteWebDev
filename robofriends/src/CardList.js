0381d05c336e05acf6fc0aad572e40b89729e274
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-11 19:40:27 -0600

    Finish building a react app 5 video

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -7,7 +7,7 @@ const CardList = ({ robots }) => {
     <div>
     {
       robots.map((user,i) => {
-        return <Card name={robots[i].name} house={robots[i].house} birthday={robots[i].dateOfBirth}/>
+        return <Card key={robots[i].name} name={robots[i].name} house={robots[i].house} birthday={robots[i].dateOfBirth}/>
       })
     }
     </div>

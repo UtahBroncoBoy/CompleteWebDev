bb90613155c4456500dc733a031088741623dc77
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-16 16:54:56 -0600

    Change back to https source for robots.

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component{
   }
 
   componentDidMount() {
-    fetch(`http://hp-api.herokuapp.com/api/characters`)
+    fetch(`https://jsonplaceholder.typicode.com/users`)
       .then(response => response.json())
       .then(users => this.setState({robots: users}));
   }

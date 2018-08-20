534680e6ca01ec741060aaefca84eb82932f448a
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-20 10:44:05 -0600

    Continue practice with promises.

--- Promises/promises.js
+++ Promises/promises.js
@@ -1,13 +1,15 @@
 const urls = [
     'https://jsonplaceholder.typicode.com/users',
     'https://jsonplaceholder.typicode.com/posts',
-    'https://jsonplaceholder.typicode.com/albums',
+    'https://jsonplaceholder.typicode.com/albums'
 ]
 
 Promise.all(urls.map(url => {
-    return fetch(url).then(response => response.json())
+    return fetch(url).then(resp => resp.json())
 })).then(results => {
-    results.map(urlItem =>{
-        console.log(urlItem);
-    })
+    // results.map(urlItem =>{
+    //     console.log(urlItem);
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2]);
 }).catch(() => console.log('There has been an error fetching data.'))

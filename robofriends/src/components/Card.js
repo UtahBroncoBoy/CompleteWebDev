f7f311b5a6766b8ba9b7b5d35c8ef841b3007756
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-24 20:20:59 -0600

    Make final changes to RoboFriends App

--- robofriends/src/components/Card.js
+++ robofriends/src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({name, house, birthday}) => {
+const Card = ({name, email, phone}) => {
   return(
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img src={`https://robohash.org/${name}?200x200`} alt='robots'/>
       <div>
         <h2>{name}</h2>
-        <p>Birthday: {birthday}</p>
-        <p>House: {house}</p>
+        <p>E-Mail: {email}</p>
+        <p>Phone: {phone}</p>
       </div>
     </div>
   );
--- robofriends/src/components/CardList.js
+++ robofriends/src/components/CardList.js
@@ -7,7 +7,7 @@ const CardList = ({ robots }) => {
     <div>
     {
       robots.map((user,i) => {
-        return <Card key={robots[i].name} name={robots[i].name} house={robots[i].house} birthday={robots[i].dateOfBirth}/>
+        return <Card key={robots[i].name} name={robots[i].name} email={robots[i].email} phone={robots[i].phone}/>
       })
     }
     </div>

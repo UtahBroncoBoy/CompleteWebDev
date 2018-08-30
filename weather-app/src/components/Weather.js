68513b34e65c9c4efb577e58372cb3b0f51ae8df
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-08-29 20:53:02 -0600

    Add small details to weather area in weather app.

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -5,7 +5,7 @@ const Weather = props => (
             {props.city && props.country && props.temperature && props.humidity && props.description &&
                 <div className='alert alert-light'>
                     <p>Location: {props.city}, {props.country}</p>
-                    <p>Temperature: {props.temperature}&#176;</p>
+                    <p>Temperature: {props.temperature}&#176; F</p>
                     <p>Humidity: {props.humidity}&#37;</p>
                     <p>Conditions: {props.description}</p>
                 </div>  

21df644271a1ce7163a99f9d37615e37a4787ce8
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-10-09 16:03:33 -0600

    Finish connection to database.

--- face-rec/src/components/FaceRecognition/FaceRecognition.js
+++ face-rec/src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageURL, box }) => {
   return(
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputImage' src={imageURL} alt='Faces to Detect' width='500px' height='auto'/>;
+        <img id='inputImage' src={imageURL} width='500px' height='auto'/>
         <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
         </div>
       </div>
--- face-rec/src/components/Rank/Rank.js
+++ face-rec/src/components/Rank/Rank.js
@@ -4,10 +4,7 @@ const Rank = ({ name, entries}) => {
   return(
     <div>
       <div className='white f3'>
-        {`${name}, your current entry count is . . .`}
-      </div>
-      <div className='white f1'>
-        {entries}
+        {`${name}, you have detected ${entries} faces!!`}
       </div>
     </div>
   );

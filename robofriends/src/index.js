7a906001bc4bea64b92aad5cb1625c95c063b46c
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-10-22 16:31:47 -0600

    Add actions, constants, and reducers

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
+import { searchRobots } from './reducers';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
- <App />, document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
+
 registerServiceWorker();
